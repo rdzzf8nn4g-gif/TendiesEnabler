779847769e91a00ab2d7b34e5eaf9798e65ddd5c
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-05-27 14:36:13 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 记得在终端或工作流中赋予权限: chmod +x build.sh
+echo ">>> 清理旧文件..."
+make clean
+
+echo ">>> 正在编译 Rootful (有根) 版本..."
+make package
+
+echo ">>> 正在编译 Rootless (无根) 版本..."
+make package THEOS_PACKAGE_SCHEME=rootless
+
+echo ">>> 编译完成！"
