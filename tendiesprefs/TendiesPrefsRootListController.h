ad584879b88b9dcc1c0e95146aaf232a70d3a265
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-05-30 18:30:42 +0800

    Update and rename TendiesPrefsRootListController.h to ZonePrefsRootListController.h

--- tendiesprefs/TendiesPrefsRootListController.h
+++
@@ -1,5 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <UIKit/UIKit.h>
-
-@interface TendiesPrefsRootListController : PSListController <UIDocumentPickerDelegate>
-@end
---
+++ zoneprefs/ZonePrefsRootListController.h
@@ -0,0 +1,5 @@
+#import <Preferences/PSListController.h>
+#import <UIKit/UIKit.h>
+
+@interface ZonePrefsRootListController : PSListController <UIDocumentPickerDelegate>
+@end
